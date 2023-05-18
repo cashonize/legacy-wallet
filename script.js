90d03c2db529f809ba1c023bae9b637b91b2c1dd
Mathieu <mr-zwets@protonmail.com>
2023-05-18 19:48:01 +0200

    fix derivation path chipnet for imported wallets

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ window.importWallet = async function importWallet() {
   const seedphrase = document.querySelector('#enterSeedphrase').value;
   const selectedDerivationPath = document.querySelector('#derivationPath').value;
   const derivationPath = selectedDerivationPath == "standard"? "m/44'/145'/0'/0/0" : "m/44'/0'/0'/0/0";
+  if(selectedDerivationPath == "standard") Config.DefaultParentDerivationPath = "m/44'/145'/0'";
   const walletId = `seed:mainnet:${seedphrase}:${derivationPath}`;
   await walletClass.replaceNamed(nameWallet, walletId);
   loadWalletInfo()
